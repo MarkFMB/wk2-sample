a08e6433fd68c513ec18f5d65866a5edad0790b1
MarkFMB <markf@mburst.com>
2013-09-05 20:35:18 -0400

    With Calculator

---
+++ calculator.js
@@ -0,0 +1,28 @@
+/*
+   currrency name(s)
+   conversion rate
+   countries
+   input
+
+*/
+
+var currencyName1, 
+	currencyName2,
+	countryName1,
+	countryName2,
+	amount1,
+	amount2,
+	conversionRate;
+
+countryName1 = 'USA';
+countryName2 = 'India';
+currencyName1 = 'dollars';
+currencyName2 = 'rupees';
+conversionRate = 66.01
+
+// Calculate conversion
+
+amount1 = 300;
+amount2 = amount1 * conversionRate;
+
+
